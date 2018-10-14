467b5715b6da069e2e3e7e41b485eaeb080de228
NikolaiGrieg <raugzok@gmail.com>
2018-10-14 15:57:03 +0200

    Initial structure for multipatient page

---
+++ TherapyDashboard/Controllers/SinglePatientController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TherapyDashboard.Controllers
+{
+    public class SinglePatientController : Controller
+    {
+        // GET: SinglePatient
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
