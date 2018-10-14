ff7d6f0b06987746d10d84c02fae28788d35e4db
NikolaiGrieg <raugzok@gmail.com>
2018-10-14 13:53:32 +0200

    Add project files.

---
+++ TherapyDashboard/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TherapyDashboard
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
