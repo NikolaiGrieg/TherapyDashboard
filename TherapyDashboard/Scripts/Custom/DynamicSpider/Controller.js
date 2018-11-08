291e0cba88d2f2ecce5946d58fbcc1e95cfe0faf
NikolaiGrieg <raugzok@gmail.com>
2018-11-08 13:49:52 +0100

    Implemented sorting on column headers in master view

--- TherapyDashboard/Scripts/Custom/DynamicSpider/Controller.js
+++ TherapyDashboard/Scripts/Custom/DynamicSpider/Controller.js
@@ -8,16 +8,17 @@ function update(index){
 
 function selectAxis(axis){
 	console.log(axis);
-	var selectedCategoryLine = new LineChart("#selectedCategory", this, axis, axis, height=250)
+	var height = 250;
+	var selectedCategoryLine = new LineChart("#selectedCategory", this, axis, axis, height=height)
 
 	//TODO make button actually remove chart
 	//TODO fix positioning in the html, possibly make linecharts less wide
 	var btn = document.createElement("BUTTON");        
-	var t = document.createTextNode("Remove");
+	var t = document.createTextNode("Remove " + axis);
 	btn.classList.add('btn');       
 	btn.classList.add('btn-primary');
 	btn.appendChild(t);
-	btn.style.marginTop = "100px"; //TODO should be about half of chart height, or some better way to inject                               
+	btn.style.marginTop = height/1.5 +"px"; //TODO include chart margins here                              
 	btn.onclick = function(){
 		d3.select("#lineChart" + axis).remove();
 		this.remove();
