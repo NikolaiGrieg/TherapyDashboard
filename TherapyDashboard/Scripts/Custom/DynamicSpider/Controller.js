a78b525d8713fe4c33bc09bd8f0e59d1b6f0a0d9
NikolaiGrieg <raugzok@gmail.com>
2018-11-15 11:05:00 +0100

    Created appendable barchart for detail view + refactoring of js chart folders

--- TherapyDashboard/Scripts/Custom/DynamicSpider/Controller.js
+++ TherapyDashboard/Scripts/Custom/DynamicSpider/Controller.js
@@ -35,45 +35,8 @@ function selectAxis(parent, axis){
 }
 
 function createBarChart(parent){
+	var container = document.getElementById(parent);
+	$("#" + parent).append("<div id='barChart'></div>");
 
-	Highcharts.chart(parent, {
-	    chart: {
-	        type: 'column'
-	    },
-	    title: {
-	        text: null
-	    },
-	    yAxis: {
-	        title: {
-	            text: 'Times logged in'
-	        }
-	    },
-	    xAxis: {
-	        title: {
-	            text: 'Week'
-	        }
-	    },
-
-	    plotOptions: {
-	        series: {
-	            label: {
-	                connectorAllowed: false
-	            },
-	            pointStart: 32
-	        }
-	    },
-
-	    series: [{
-	        name: 'Logins',
-	        data: [3, 0, 1, 2, 0, 4, 1, 1]
-	    }],
-
-	    legend: {
-	        enabled: false
-	    },
-
-	    credits: {
-	        enabled: false
-	    },
-	});
+	barChart = new BarChart('barChart', this)
 }
