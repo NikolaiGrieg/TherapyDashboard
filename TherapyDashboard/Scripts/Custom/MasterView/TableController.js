a5f508aca21e92d09a33bad5293ed9962fe129fc
NikolaiGrieg <raugzok@gmail.com>
2018-11-15 18:35:21 +0100

    Implemented filtering through search and checked for master table

---
+++ TherapyDashboard/Scripts/Custom/MasterView/TableController.js
@@ -0,0 +1,63 @@
+﻿function initTable() {
+    enableTableSort();
+    initSearch();
+}
+
+function enableTableSort() {
+    const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+    const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+        v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+    //TODO doesn't work for the first element in the table
+    document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+        const table = document.getElementById("masterTable");
+        Array.from(table.tBodies[0].querySelectorAll('tr:nth-child(n+1)'))
+            .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+            .forEach(tr => table.tBodies[0].appendChild(tr));
+    })));
+}
+
+function initSearch(){
+    $(function(){
+      $("#patientFilter").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#masterTableBody tr").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+    });
+}
+
+function clearFilter() {
+    $(function(){
+        $("#masterTableBody tr").filter(function() {
+            this.style.display = "table-row"
+        });
+    });
+}
+
+function filterDone() {
+    $(function(){
+        var threshold = 7;
+        $("#masterTableBody tr").filter(function() {
+            var colSixVal = this.children[6]
+            var lastCheckedCurrent = parseInt(colSixVal.innerText[0])
+            $(this).toggle(lastCheckedCurrent > threshold)
+        });
+    });
+}
+
+function filterNotDone() {
+    $(function(){
+        var threshold = 7;
+        $("#masterTableBody tr").filter(function() {
+            var colSixVal = this.children[6]
+            var lastCheckedCurrent = parseInt(colSixVal.innerText[0])
+            $(this).toggle(lastCheckedCurrent <= threshold)
+        });
+    });
+}
+
+initTable();
