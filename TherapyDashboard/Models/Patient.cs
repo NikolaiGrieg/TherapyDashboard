1d33fa7b73be95a892f622393f0ca6c2da48cc42
NikolaiGrieg <raugzok@gmail.com>
2018-11-20 13:00:15 +0100

    cleaning up models before creating db

--- TherapyDashboard/Models/Patient.cs
+++ TherapyDashboard/Models/Patient.cs
@@ -9,11 +9,7 @@ namespace TherapyDashboard.Models
     {
         public int id { get; set; }
         public string name { get; set; }
-        public string[] forms { get; set; } //TODO foreign key
-        public Dictionary<string, string> background { get; set; }
-        public string[] tasks { get; set; }
-        public DateTime previousDelivery { get; set; }
-        public DateTime nextDelivery { get; set; }
+
         public string Measurement1Path{ get; set; }
 
         public static Random random = null;
@@ -29,17 +25,7 @@ namespace TherapyDashboard.Models
             int num = random.Next(0, 10);
             Patient pat = new Patient() {
                 name = "Guy Simulated" + num,
-                id = num,
-                forms = new string[] { "SPS", "SIAS", "SE - SKALA", "TIC - C", "Bakgrunn" },
-                background = new Dictionary<string, string> {
-                    { "Alder", "30" },
-                    { "Kjønn", "Mann" },
-                    { "Utdanning","Videregående skole/gymnas" },
-                    { "Sivilstatus", "Enslig" },
-                    { "Antall barn", "Ingen" }
-                },
-                tasks = new string[] { "Modul 1: Skjema 1", "Modul 1: Skjema 2", "Modul 2: Skjema 1", "Modul 2: Skjema 2", "Modul 2: Skjema 3", },
-                nextDelivery = DateTime.Now
+                id = num
             };
 
             return pat;
