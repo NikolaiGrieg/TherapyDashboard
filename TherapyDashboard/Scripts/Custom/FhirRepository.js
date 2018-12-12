f652550a8bd1a8055c897ef5ce2b27c600244393
NikolaiGrieg <raugzok@gmail.com>
2018-12-12 20:37:29 +0100

    Implemented chart selectors for line/none

--- TherapyDashboard/Scripts/Custom/FhirRepository.js
+++ TherapyDashboard/Scripts/Custom/FhirRepository.js
@@ -13,12 +13,12 @@ $(function fhirData(){
 		}).then(function(results, refs){
 			console.log(results)
 		});
-		
+		*/
 		//Get all questionnaire responses
-		smart.api.search({type: "QuestionnaireResponse"}).then(function(results, refs){
+		smart.api.search({type: "QuestionnaireResponse"}).then(results =>{
 			console.log(results)
 		});
-		*/
+		
 	    smart.api.fetchAllWithReferences({ type: "Observation" }).then(function (results, refs) {
 	    	//TODO get all patients for main page
 	        results.forEach(function (obs) {
@@ -43,26 +43,124 @@ $(function fhirData(){
 	            
 	            data.push(entry)
 	        });
-	        fhirCharts(data)
+	        fhirCharts(data);
 	    });
 	});
 })
 
+function initCheckBoxes(names){
+	var container = document.getElementById("observationsContainer");
 
-//TODO check boxes to select available measurements
-//TODO Circles on data points in line chart
-function fhirCharts(data){
+	for (let i = 0; i < names.length; i++){
+		var selectorContainer = document.createElement("div");
 
-	//Find unique measurements
-	var measurements = []
+		/*
+		var checkbox = document.createElement('input');
+		checkbox.type = "checkbox";
+		checkbox.id = "obs" + i;
+		checkbox.value = "true";
+		checkbox.setAttribute("onClick", "toggleChart(this)");
+
+		var label = document.createElement('label')
+		label.htmlFor = "obs" + i;
+		label.appendChild(document.createTextNode(names[i]));
+
+		selectorContainer.appendChild(checkbox);
+		selectorContainer.appendChild(label);
+		*/
+		var html = `
+			<div class="btn-group">
+			<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			${names[i]} 
+			<span class="caret"></span>
+			</button> 
+			<ul class="dropdown-menu"> 
+			<li>
+			    <a href="#" onclick="lineChart(this)" id="lineFor${names[i]}">Line</a>
+			</li> 
+			<li>
+			    <a href="#" onclick="frequencyChart(this)" id="frequencyFor${names[i]}">Frequency</a>
+			</li> 
+			<li>
+			    <a href="#" onclick="eventChart(this)" id="eventFor${names[i]}">Events</a>
+			</li>  
+			<li>
+			    <a href="#" onclick="disableChart(this)" id="removeFor${names[i]}">None</a>
+			</li> 
+			</ul> 
+			</div>
+			`;
+
+		var template = document.createElement('template');
+	    html = html.trim(); 
+	    template.innerHTML = html;
+
+		var header = document.createElement("h5");
+		header.textContent = names[i]
+
+		//selectorContainer.appendChild(header);
+		selectorContainer.appendChild(template);
+
+
+		selectorContainer.innerHTML = html;
+		container.appendChild(selectorContainer);
+	}
+
+
+	
+}
+const getMeasurementNames = data =>{
+	let measurements = []
 	for (let i = 0; i < data.length; i++){
 		if (!measurements.includes(data[i].measurement)){
 			measurements.push(data[i].measurement)
 		}
 	}
+	return measurements
+}
+
+function lineChart(a) {
+	let name = a.id.replace("lineFor", "");
+	let linechart = disabledCharts[name];
+
+	var parent = document.getElementById("line");
+	parent.appendChild(linechart);
+}
+
+function frequencyChart(a){
+	//TODO
+}
+
+function eventChart(a){
+	//TODO
+}
+
+var disabledCharts = {}
+
+function disableChart(a){
+	//TODO just set inactive, don't actually remove
+	let name = a.id.replace("removeFor", "")
+	let divName = name.replace(/\s/g, ''); //remove spaces to match correct div
+	let containerName = "lineChart" + divName
+	var chart = document.getElementById(containerName);
+
+	disabledCharts[name] = chart;
+
+	var parent = chart.parentNode;
+	parent.removeChild(chart);
+
+}
+
+
+//TODO get background information from patient
+//available data should contain: Age, gender, maritalStatus, language(?), missing module, children, education
+function fhirCharts(data){
+
+	//Find unique measurements
+	let measurements = getMeasurementNames(data);
 	//console.log(measurements)
 
-	
+	let filteredMeasurements = [];
 	for (let i = 0; i < measurements.length; i++){
 		var cleaned = []
 		var measurement = measurements[i]
@@ -73,11 +171,6 @@ function fhirCharts(data){
 			if (observation.quantity){
 				//console.log(observation)
 				cleaned.push(observation)
-				/*
-				if (!names.includes(name)){
-					names.push(name);
-				}
-				*/
 			}
 		}
 		
@@ -99,12 +192,15 @@ function fhirCharts(data){
 			}
 		}
 
-		//Line chart with 1 data point doesn't help much
+		//remove observations with 1 data point
 		if (Object.keys(categorized).length > 1){
-			linechart = new LineChart("#line", this, measurement, 'all', categorized, 200, 700, fhir=true);
+			filteredMeasurements.push(measurement)
+			let linechart = new LineChart("#line", this, measurement, 'all', categorized, 200, 700, fhir=true);
+			//charts[measurement] = linechart;
 		}
 		
 	}
+	initCheckBoxes(filteredMeasurements);
 
 	
 }
