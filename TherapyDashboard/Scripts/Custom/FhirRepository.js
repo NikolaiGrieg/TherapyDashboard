82c5364d6e0149691f7d6ea1f50ff2e462d2a940
NikolaiGrieg <raugzok@gmail.com>
2018-12-13 17:02:38 +0100

    Improved dropdown buttons for selecting observation charts

--- TherapyDashboard/Scripts/Custom/FhirRepository.js
+++ TherapyDashboard/Scripts/Custom/FhirRepository.js
@@ -14,6 +14,7 @@ $(function fhirData(){
 			console.log(results)
 		});
 		*/
+		
 		//Get all questionnaire responses
 		smart.api.search({type: "QuestionnaireResponse"}).then(results =>{
 			console.log(results)
@@ -48,53 +49,94 @@ $(function fhirData(){
 	});
 })
 
-function initCheckBoxes(names){
+function initSelectors(measurements){
+	let names = Object.keys(measurements);
+
 	var container = document.getElementById("observationsContainer");
 
-	for (let i = 0; i < names.length; i++){
-		var selectorContainer = document.createElement("div");
+	var listItems = "";
 
-		var html = `
+	for (let i = 0; i < names.length; i++){
+		let curMeasurement = filteredMeasurements[names[i]];
+		let numObs = Object.keys(curMeasurement).length;
+
+		var currentHTML = `
+			<li>
+			    <a href="#" onclick="createChartSelector(this)" id="selectorFor${names[i]}">${names[i]} (${numObs})</a>
+			</li>
+		`
+		listItems += currentHTML;
+	}
+
+	//TODO have button take the name of clicked observation
+	var html = `
 			<div class="btn-group">
-			<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			${names[i]} 
+			<button type="button" class="btn btn-info btn-block dropdown-toggle" 
+			data-toggle="dropdown" id="observationSelectorBtn" aria-haspopup="true" aria-expanded="false">
+			Select Observation
+			<span class="caret"></span>
+			</button> 
+			<ul class="dropdown-menu" style="position: absolute;"> 
+			${listItems}
+			</ul> 
+			</div>
+			`;
+	
+
+	var observationsDropdown = document.createElement("div");
+	observationsDropdown.innerHTML = html;
+	container.appendChild(observationsDropdown);
+
+}
+
+function createChartSelector(obsSelector) {
+	let name = obsSelector.id.replace("selectorFor", "");
+	var container = document.getElementById("observationsContainer");
+
+	//Logic for determining chart options go here
+
+	//create chart selector
+	var selectorContainer = document.createElement("div");
+	selectorContainer.id = "selectorContainerFor" + name;
+
+	var html = `
+			<div class="btn-group">
+			<button type="button" class="btn btn-info btn-block dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			Select Chart
 			<span class="caret"></span>
 			</button> 
 			<ul class="dropdown-menu"> 
 			<li>
-			    <a href="#" onclick="lineChart(this)" id="lineFor${names[i]}">Line</a>
+			    <a href="#" onclick="lineChart(this)" id="lineFor${name}">Line</a>
 			</li> 
 			<li>
-			    <a href="#" onclick="frequencyChart(this)" id="frequencyFor${names[i]}">Frequency</a>
+			    <a href="#" onclick="frequencyChart(this)" id="frequencyFor${name}">Frequency</a>
 			</li> 
 			<li>
-			    <a href="#" onclick="eventChart(this)" id="eventFor${names[i]}">Events</a>
+			    <a href="#" onclick="eventChart(this)" id="eventFor${name}">Events</a>
 			</li>  
 			<li>
-			    <a href="#" onclick="disableChart(this)" id="removeFor${names[i]}">None</a>
+			    <a href="#" onclick="disableChart(this)" id="removeFor${name}">None</a>
 			</li> 
 			</ul> 
 			</div>
 			`;
 
-		var template = document.createElement('template');
-	    html = html.trim(); 
-	    template.innerHTML = html;
+	selectorContainer.innerHTML = html;
+	container.appendChild(selectorContainer);
 
-		var header = document.createElement("h5");
-		header.textContent = names[i]
-
-		//selectorContainer.appendChild(header);
-		selectorContainer.appendChild(template);
-
-
-		selectorContainer.innerHTML = html;
-		container.appendChild(selectorContainer);
-	}
-
-
-	
+	changeSelectedObservationBtnText(name);
 }
+
+function changeSelectedObservationBtnText(text){
+	let btn = document.getElementById("observationSelectorBtn");
+	//let curText = btn.innerText;
+	//console.log(curText);
+
+	btn.innerHTML = text + `<span class="caret"></span>`;
+}
+
+
 const getMeasurementNames = data =>{
 	let measurements = []
 	for (let i = 0; i < data.length; i++){
@@ -116,6 +158,13 @@ function lineChart(a) {
 	else{
 		linechart = new LineChart("#line", this, name, 'all', filteredMeasurements[name], 200, 700, fhir=true);
 	}
+
+	//clean up
+	//destroy selector button
+	let selector = document.getElementById("selectorContainerFor" + name);
+	selector.parentNode.removeChild(selector);
+
+	changeSelectedObservationBtnText("Select Chart")
 	
 }
 
@@ -141,6 +190,11 @@ function disableChart(a){
 	var parent = chart.parentNode;
 	parent.removeChild(chart);
 
+	//destroy selector button
+	let selector = document.getElementById("selectorContainerFor" + name);
+	selector.parentNode.removeChild(selector);
+
+	changeSelectedObservationBtnText("Select Chart")
 }
 
 var filteredMeasurements = {};
@@ -187,7 +241,7 @@ function fhirCharts(data){
 		}
 		
 	}
-	initCheckBoxes(Object.keys(filteredMeasurements));
+	initSelectors(filteredMeasurements);
 
 	
 }
